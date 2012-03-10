5c42f120e46d552871df0b257c7275ac57016888
Iivari Äikäs <iivari.aikas@gmail.com>
2012-03-10 19:00:34 +0200

    api#get mockup

--- src/dungeon/routes.clj
+++ src/dungeon/routes.clj
@@ -1,11 +1,29 @@
 (ns dungeon.routes
   (:use compojure.core
         dungeon.views
+        [dungeon.map :as map]
         [hiccup.middleware :only (wrap-base-url)])
   (:require [compojure.route :as route]
-            [compojure.handler :as handler]))
+            [compojure.handler :as handler]
+            [clojure.data.json :as json]))
+
+(defn blob [game-state]
+  (for [column (range (width game-state))
+        row (range (height game-state))]
+    (let [tile (tile-at game-state [column row])]
+      {:x column :y row :type tile})))
+
+
+(defn map->json [map-state]
+  (let [width (map/width map-state)
+        height (map/height map-state)
+        contents (blob map-state)]
+    (json/json-str {:width width :height height :contents contents})))
+
 
 (defroutes player-routes
+  (GET "/" [] (map->json (map/read-map "..@.."))) ; Get current map information
+  (PUT "/" [] ()) ; Move player to given location
   (route/not-found "duh"))
 
 (defroutes dungeon-routes
