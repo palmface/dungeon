1cb1e6afd978dfa19be0c12cc079917c1fa211f5
Jani Rahkola <jani.rahkola@gmail.com>
2012-03-10 20:23:04 +0200

    tests for dungeon.utils/some-indexed

--- src/dungeon/utils.clj
+++ src/dungeon/utils.clj
@@ -1,5 +1,20 @@
-(ns dungeon.utils)
+(ns dungeon.utils
+  (:use [midje.sweet :as t]))
 
 (defn some-indexed [pred coll]
   (some (fn [[i x]] (pred i x))
         (map-indexed vector coll)))
+
+(t/fact
+ (some-indexed (fn [i x]
+                 (if (= i 1)
+                   x))
+               ['a 'b 'c]) => 'b
+ (some-indexed (fn [i x]
+                 (if (= x 'c)
+                   i))
+               ['a 'b 'c]) => 2
+ (some-indexed (fn [i x]
+                 (if (= i x)
+                   true))
+               ['a 'b 'c]) => nil)
