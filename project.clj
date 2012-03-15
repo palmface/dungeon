9881a5f40eb59db9bf301645592b2fd01c33070e
Jani Rahkola <jani.rahkola@gmail.com>
2012-03-15 02:03:01 +0200

    Upgraded project.clj to Leiningen 2.0.
    Run lein upgrade with required permission to upgrade Leiningen.
    Project.clj should also work with 1.7.0.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject dungeon "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject dungeon "0.1.0"
+  :description "roguelike with a web client"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.1"]
                  [hiccup "0.3.7"]
-                 [org.clojure/data.json "0.1.2"]]
-  :dev-dependencies [[midje "1.3.1"]
-                     [lein-midje "1.0.8"]
-                     [lein-ring "0.5.4"]]
+                 [org.clojure/data.json "0.1.2"]
+                 [midje "1.3.0"]]
+  :plugins [[org.clojars.the-kenny/lein-midje "1.0.9"]
+            [lein-ring "0.6.1"]]
   :ring {:handler dungeon.routes/dung}
   :main dungeon.core)
